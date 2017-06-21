d0c380660a26a07ddf65923845146deab189de73
Tomo Krajina <tkrajina@gmail.com>
2017-06-21 21:20:50 +0200

    Expose TagParser

--- reflector/reflector.go
+++ reflector/reflector.go
@@ -3,7 +3,6 @@ package reflector
 import (
 	"fmt"
 	"reflect"
-	"strconv"
 	"strings"
 )
 
@@ -421,57 +420,8 @@ func (of *ObjField) Tags() (map[string]string, error) {
 		return nil, err
 	}
 
-	res := map[string]string{}
 	tag := of.structField.Tag
-
-	// This code is copied/modified from: reflect/type.go:
-	for tag != "" {
-		// Skip leading space.
-		i := 0
-		for i < len(tag) && tag[i] == ' ' {
-			i++
-		}
-		tag = tag[i:]
-		if tag == "" {
-			break
-		}
-
-		// Scan to colon. A space, a quote or a control character is a syntax error.
-		// Strictly speaking, control chars include the range [0x7f, 0x9f], not just
-		// [0x00, 0x1f], but in practice, we ignore the multi-byte control characters
-		// as it is simpler to inspect the tag's bytes than the tag's runes.
-		i = 0
-		for i < len(tag) && tag[i] > ' ' && tag[i] != ':' && tag[i] != '"' && tag[i] != 0x7f {
-			i++
-		}
-		if i == 0 || i+1 >= len(tag) || tag[i] != ':' || tag[i+1] != '"' {
-			break
-		}
-		name := string(tag[:i])
-		tag = tag[i+1:]
-
-		// Scan quoted string to find value.
-		i = 1
-		for i < len(tag) && tag[i] != '"' {
-			if tag[i] == '\\' {
-				i++
-			}
-			i++
-		}
-		if i >= len(tag) {
-			break
-		}
-		qvalue := string(tag[:i+1])
-		tag = tag[i+1:]
-
-		value, err := strconv.Unquote(qvalue)
-		if err != nil {
-			return nil, fmt.Errorf("Cannot unquote tag %s in %T.%s: %s", name, of.obj.iface, of.name, err.Error())
-		}
-		res[name] = value
-	}
-
-	return res, nil
+	return ParseTag(string(tag))
 }
 
 // TagExpanded returns the tag value "expanded" with commas.
